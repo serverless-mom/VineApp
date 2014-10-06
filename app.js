cada1660740773023cd199c691130ee1d52ab793
tobyfee <tobyfee>
2014-10-06 15:35:18 -0700

    blocked by vine api

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
 /**
  * Created by toby on 10/4/14.
  */
+var vineApp = angular.module('vineApp', []);
 
 
-function customersController($scope,$http) {
-    $http({
-        method: 'GET',
-        url: 'json/catVines.json'
-    }).success(function(data) {
-        $scope.vines = data.data.records; // response data
-        $scope.albums = [];
-    });
-}
+vineApp.config(function($httpProvider) {
+    //Enable cross domain calls
+    $httpProvider.defaults.useXDomain = true;
+    delete $httpProvider.defaults.headers.common['X-Requested-With'];
+});
+
+vineApp.controller('customersController',function($scope,$http){
+    $http({method: 'GET', url: 'json/catVines.json'}).
+        success(function(data, status, headers, config) {
+            $scope.vines = data.data.records;
+        }).
+        error(function(data, status, headers, config) {
+            console.log("Some kind of error when requesting the doc, we got back "+status+data);
+        });
+});
+
